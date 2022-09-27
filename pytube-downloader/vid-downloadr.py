94f48fac7d66fa9e72e97378740d6fa1ecd17117
ulugbekbahromov <ulugbekbahromov@gmail.com>
2022-09-27 16:49:55 +0500

    write function to download yt video

---
+++ pytube-downloader/vid-downloadr.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+def download_vid(yt):
+    # filter mp4 streams from object
+    my_streams = yt.streams.filter(file_extension='mp4', only_video=True)
+
+    for streams in my_streams:
+        print(f"Video itag : {streams.itag} Resolution : {streams.resolution}")
+
+    
+    input_itag = input("Enter itag value : ")
+    video = yt.streams.get_by_itag(input_itag)
+
+    video.download("pytube-downloader")
+    print("Vid is downloading as\"", yt.title + ".mp4\"")
+
+
+link = input("Enter your link here : ")
+yt = YouTube(link)
+download_vid(yt)
